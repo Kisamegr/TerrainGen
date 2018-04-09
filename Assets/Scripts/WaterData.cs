47cb0042f92a4ffb9d0f59ccf840611349be6186
Stratos Makrigiannakis <stratos.makrigiannakis@gmail.com>
2018-04-09 17:37:08 +0300

    -Moved the terrain code unto a submodule -Started the voxel engine

--- Assets/Scripts/WaterData.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-
-[CreateAssetMenu(fileName = "New WaterData", menuName = "Water Data")]
-public class WaterData : ScriptableObject {
-  [Header("Water Properties")]
-  public int resolution = 256;
-}
